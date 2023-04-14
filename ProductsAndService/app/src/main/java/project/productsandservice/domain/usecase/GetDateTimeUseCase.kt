6248cb094a0de71cbf0fde0e12cb8b471582a24e
Andestand <110184532+Andestand@users.noreply.github.com>
2023-04-14 21:14:23 +0300

    UseCase для получения точного времени

---
+++ ProductsAndService/app/src/main/java/project/productsandservice/domain/usecase/GetDateTimeUseCase.kt
@@ -0,0 +1,20 @@
+package project.productsandservice.domain.usecase
+
+import android.annotation.SuppressLint
+import android.os.Build
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class GetDateTimeUseCase {
+
+    fun execute(): String {
+        return dataAndTime()
+    }
+
+    @SuppressLint("NewApi")
+    private fun dataAndTime(): String {
+        val dateTime = LocalDateTime.now()
+        val format = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")
+        return dateTime.format(format)
+    }
+}

e0591dbe840710eece1ebe08a1a9fb76998807c8
Andestand <110184532+Andestand@users.noreply.github.com>
2023-09-14 23:21:43 +0300

    добавление фиксирования текущего времени в UseCase

--- WordAccounting/app/src/main/java/ru/project/workaccounting/domain/usecase/GetDateTimeUseCase.kt
+++ WordAccounting/app/src/main/java/ru/project/workaccounting/domain/usecase/GetDateTimeUseCase.kt
@@ -1,15 +1,45 @@
 package ru.project.workaccounting.domain.usecase
 
 import android.annotation.SuppressLint
+import ru.project.workaccounting.domain.models.DateTime
+import ru.project.workaccounting.ui.reports.DateTimeConverter
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class GetDateTimeUseCase {
+    val f = DateTimeConverter()
 
-    fun execute(): String = get()
+    fun execute(): DateTime = get()
 
     @SuppressLint("NewApi")
-    private fun get(): String = LocalDateTime.now().format(
-        DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")
-    )
+    private fun get(): DateTime {
+        val day = LocalDateTime.now().format(
+            DateTimeFormatter.ofPattern("dd")
+        )
+
+        val month = LocalDateTime.now().format(
+            DateTimeFormatter.ofPattern("MM")
+        )
+
+        val year = LocalDateTime.now().format(
+            DateTimeFormatter.ofPattern("yyyy")
+        )
+
+        val hour = LocalDateTime.now().format(
+            DateTimeFormatter.ofPattern("HH")
+        )
+
+        val minutes = LocalDateTime.now().format(
+            DateTimeFormatter.ofPattern("mm")
+        )
+
+        return DateTime(
+            day = day,
+            idMonth = month,
+            month = null,
+            year = year,
+            hour = hour,
+            minutes = minutes
+        )
+    }
 }
